da0e90564538efc843c03b983b289b9c479921de
Cassandra Beauchamp <cassiewithstars@gmail.com>
2023-11-12 16:09:48 -0600

    Added maxInv and minInv fields, added a all fields constructor, and getters and setters for them

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -28,6 +28,10 @@ public abstract class Part implements Serializable {
     double price;
     @Min(value = 0, message = "Inventory value must be positive")
     int inv;
+    @Min(value = 0, message = "Inventory value must be positive")
+    int maxInv;
+    @Min(value = 0, message = "Inventory value must be positive")
+    int minInv;
 
     @ManyToMany
     @JoinTable(name="product_part", joinColumns = @JoinColumn(name="part_id"),
@@ -49,7 +53,19 @@ public abstract class Part implements Serializable {
         this.price = price;
         this.inv = inv;
     }
+    public Part(long id, String name, double price, int inv, int maxInv, int minInv) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.inv = inv;
+        this.maxInv = maxInv;
+        this.minInv= minInv;
+    }
 
+    public int getMaxInv(){return maxInv;}
+    public int getMinInv(){return minInv;}
+    public void setMaxInv(int maxInv){this.maxInv = maxInv;}
+    public void setMinInv(int minInv){this.minInv = minInv;}
     public long getId() {
         return id;
     }
